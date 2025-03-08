ee5a8f5a84e283e37ef027c6fe5dacdc2d33d6e4
Rasmus Aasmäe <s2031389@ed.ac.uk>
2025-03-08 11:07:09 +0200

    init spring boot and nextjs projects

---
+++ backend/src/test/java/com/aasmae/exchange/ExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aasmae.exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
